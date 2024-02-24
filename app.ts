35bae60f1a02de31fe513e3d6e45d96f59582d7a
FarahMahfooz <160307793+FarahMahfooz@users.noreply.github.com>
2024-02-24 08:00:16 -0800

    upload file

---
+++ app.ts
@@ -0,0 +1,9 @@
+// lower case
+let personName: string ="Farah"
+console.log("lowercase:",personName.toLowerCase());
+
+// uppercase
+console.log("uppercase:",personName.toLocaleUpperCase());
+
+// titlecase
+console.log("titlecase:", personName.replace(/\bw/g,c => c.toUpperCase()));
